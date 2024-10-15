74f0c5b546545d5123d8490687791be5127a083d
jorbson_luiz <jluiz_silva01@hotmail.com>
2024-10-15 00:51:35 -0300

    Ajustes

--- valor_dollar.py
+++ valor_dollar.py
@@ -13,28 +13,5 @@ if response.status_code == 200:
     valor = texto.split("=")[1].split()[0]
     print(f"O dollar está {valor} Reais")
 
-    
-
-    # user = []
-    
-    # user= [{
-    #     "nome":usuario,
-    #     "senha":senha
-    # }]
-    # flash('usuario cadastrado')
-    # with open('usuarios.json') as uriarios:
-    #     arquivo = json.load(uriarios)
-    # arquivo2 = arquivo + user
-    # with open('usuarios.json', 'w') as arquivo:
-    #     json.dump(arquivo2, arquivo, indent=4)
-    # return render_template("html/acesso.html", nomecadastro=usuario + " cadastrado!!")
-
-
-
-
-
-
-
-
 else:
     print(f"Ocorreu um erro ao fazer a requisição. Status code: {response.status_code}")
